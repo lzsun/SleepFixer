036e6b3b50c46188f5a37c891c8db6e871a33887
fan89 <chen.fan89@gmail.com>
2013-05-08 17:54:20 -0700

    code initial upload

---
+++ Sleep Fixer/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using Sleep_Fixer.Resources;
+
+namespace Sleep_Fixer
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
